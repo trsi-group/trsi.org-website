f4253b7c09b31c98a616fdeef314a6423d2e7408
oliver <o.kohl@sap.com>
2025-01-17 17:13:57 +0100

    card update

--- app.js
+++ app.js
@@ -36,12 +36,13 @@ getData('/cms/data/productions.json')
   for (let i in data.productions) {
     const year = data.productions[i].release_date.split("-")[0];
     html += `<div class="col">
-      <div class="card h-100">
-        <div>
-          <img class="card-img-prod img-fluid" src="${data.productions[i].image}" alt="...">
+      <div class="card h-100 border-0 bg-secondary">
+        <div class="d-flex h-100 align-items-center">
+          <img class="card-img-top img-fluid" src="${image}" alt="...">
         </div>
         <div class="card-body">
-          <p class="card-text c64-purple">${data.productions[i].title} (${year})</p>
+          <h5 class="card-text c64-yellow">${data.productions[i].title} (${year})</h5>
+          <p class="card-text c64-yellow">${data.productions[i].description}</p>
         </div>
       </div>
     </div>`;
@@ -52,7 +53,32 @@ getData('/cms/data/productions.json')
     console.log(err);
 });
 
-getData('data/members.json')
+getData('/cms/data/graphics.json')
+.then(data => {
+  const graphicsRoot = document.getElementById('graphics-container');
+  let html = '';
+  
+  console.log(data);
+  for (let i in data.graphics) {
+    const image = adjustImagePath(data.graphics[i].image || "placeholder.webp", "card");
+    html += `<div class="col">
+      <div class="card h-100 border-0 bg-secondary">
+        <div>
+          <img class="card-img-top img-fluid" src="${image}" alt="...">
+        </div>
+        <div class="card-body">
+          <p class="card-text c64-yellow">${data.graphics[i].title})</p>
+        </div>
+      </div>
+    </div>`;
+  }
+  graphicsRoot.innerHTML = html;
+})
+.catch(err => {
+    console.log(err);
+});
+
+getData('/cms/data/members.json')
 .then(data => {
   const membersRoot = document.getElementById('members-container');
   let html = '';
@@ -60,12 +86,12 @@ getData('data/members.json')
   console.log(data);
   for (let i in data.members) {
     html += `<div class="col">
-      <div class="card h-100">
+      <div class="card h-100 border-0 bg-secondary">
         <div>
-          <img class="card-img-prod img-fluid" src="${data.members[i].avatar}" alt="...">
+          <img class="card-img-top img-fluid" src="${image}" alt="...">
         </div>
         <div class="card-body">
-          <p class="card-text c64-purple">${data.members[i].title}</p>
+          <p class="card-text c64-yellow">${data.members[i].handle} (${data.members[i].member_status})</p>
         </div>
       </div>
     </div>`;
