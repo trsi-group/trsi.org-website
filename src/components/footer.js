933109dbe35ed19e95a6eb5502c77f4eae773821
oliver <o.kohl@sap.com>
2025-01-24 12:26:28 +0100

    minor updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return `
     <footer id="footer-section" class="container-fluid py-3">
-      <p class="text-center text-primary">The sleeping gods 2025</p>
+      <p class="text-center text-uppercase c64-white h5">The sleeping gods 2025</p>
     </footer>
   `;
 }
