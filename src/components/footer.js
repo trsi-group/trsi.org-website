15b45eaae2b687a1fd8575b7d721ae4ffae4d318
oliver <o.kohl@sap.com>
2025-02-01 18:36:49 +0100

    icon spacing and styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,10 +9,10 @@ export default function Footer() {
             <p class="text-center text-uppercase c64-white h5">The sleeping gods 2025</p>
           </div>
           <div class="col">
-            <a href="https://www.facebook.com/tristarandredsector" target="_blank"><i class="bi-facebook" style="font-size: 1.5rem; color: white;"></i></a>
-            <a href="https://x.com/trsi" target="_blank"><i class="bi-twitter-x" style="font-size: 1.5rem; color: white;"></i></a>
-            <a href="https://www.instagram.com/_trsi_" target="_blank"><i class="bi-instagram" style="font-size: 1.5rem; color: white;"></i></a>
-            <a href="https://youtube.com/@trsi" target="_blank"><i class="bi-youtube" style="font-size: 1.5rem; color: white;"></i></a>
+            <a href="https://www.facebook.com/tristarandredsector" class="me-2" target="_blank"><i class="bi-facebook social-icon"></i></a>
+            <a href="https://x.com/trsi" class="me-2" target="_blank"><i class="bi-twitter-x social-icon"></i></a>
+            <a href="https://www.instagram.com/_trsi_" class="me-2" target="_blank"><i class="bi-instagram social-icon"></i></a>
+            <a href="https://youtube.com/@trsi" target="_blank"><i class="bi-youtube social-icon"></i></a>
           </div>
         </div>
       </div>
