a5072ab7634426a804f38882e68f22b1032a8932
oliver <o.kohl@sap.com>
2025-02-01 18:45:21 +0100

    responsive columns for social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,17 +2,17 @@ export default function Footer() {
   return `
     <footer id="footer-section" class="container-fluid py-3">
       <div class="container text-center">
-        <div class="row align-items-start">
-          <div class="col">
-          </div>
-          <div class="col">
-            <p class="text-center text-uppercase c64-white h5">The sleeping gods 2025</p>
-          </div>
+        <div class="row row-cols-1 row-cols-md-3 align-items-start">
           <div class="col">
             <a href="https://www.facebook.com/tristarandredsector" class="me-2" target="_blank"><i class="bi-facebook social-icon"></i></a>
             <a href="https://x.com/trsi" class="me-2" target="_blank"><i class="bi-twitter-x social-icon"></i></a>
             <a href="https://www.instagram.com/_trsi_" class="me-2" target="_blank"><i class="bi-instagram social-icon"></i></a>
             <a href="https://youtube.com/@trsi" target="_blank"><i class="bi-youtube social-icon"></i></a>
+          </div>
+          <div class="col">
+            <p class="text-center text-uppercase c64-white h5">The sleeping gods 2025</p>
+          </div>
+          <div class="col">
           </div>
         </div>
       </div>
